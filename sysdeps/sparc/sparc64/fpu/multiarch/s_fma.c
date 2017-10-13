e7609f89e932fcfc561cc737dcc357e8818684ef
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2017-10-13 13:33:33 -0300

    sparc: hide internal __fma{f}_{vis3,generic}
    	* sysdeps/sparc/sparc64/fpu/multiarch/s_fma.c (__ceil_vis3,
    	__ceil_generic): Use attribute_hidden.
    	* sysdeps/sparc/sparc64/fpu/multiarch/s_fmaf.c (__ceilf_vis3,
    	__ceilf_generic): Likewise.

--- sysdeps/sparc/sparc64/fpu/multiarch/s_fma.c
+++ sysdeps/sparc/sparc64/fpu/multiarch/s_fma.c
@@ -1,8 +1,11 @@
-#include <sparc-ifunc.h>
 #include <math.h>
+#include <sparc-ifunc.h>
 
-extern double __fma_vis3 (double, double, double);
-extern double __fma_generic (double, double, double);
+extern __typeof (fma) __fma_vis3 attribute_hidden;
+extern __typeof (fma) __fma_generic attribute_hidden;
 
-sparc_libm_ifunc(__fma, hwcap & HWCAP_SPARC_FMAF ? __fma_vis3 : __fma_generic);
+sparc_libm_ifunc (__fma,
+		  hwcap & HWCAP_SPARC_FMAF
+		  ? __fma_vis3
+		  : __fma_generic);
 weak_alias (__fma, fma)
--- sysdeps/sparc/sparc64/fpu/multiarch/s_fmaf.c
+++ sysdeps/sparc/sparc64/fpu/multiarch/s_fmaf.c
@@ -1,8 +1,11 @@
-#include <sparc-ifunc.h>
 #include <math.h>
+#include <sparc-ifunc.h>
 
-extern float __fmaf_vis3 (float, float, float);
-extern float __fmaf_generic (float, float, float);
+extern __typeof (fmaf) __fmaf_vis3 attribute_hidden;
+extern __typeof (fmaf) __fmaf_generic attribute_hidden;
 
-sparc_libm_ifunc(__fmaf, hwcap & HWCAP_SPARC_FMAF ? __fmaf_vis3 : __fmaf_generic);
+sparc_libm_ifunc (__fmaf,
+		  hwcap & HWCAP_SPARC_FMAF
+		  ? __fmaf_vis3
+		  : __fmaf_generic);
 weak_alias (__fmaf, fmaf)
